1cc74889ae8c1eb4e55c6366ebf34293e0af53cb
Waseem <wasim_ahmad2003@yahoo.com>
2024-08-10 15:05:55 +0500

    re-configured the app

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,15 +1,15 @@
-import { HardhatUserConfig } from "hardhat/config"
-import "@typechain/hardhat"
-import "@nomiclabs/hardhat-waffle"
-import "@nomiclabs/hardhat-etherscan"
-import "@nomiclabs/hardhat-ethers"
-import "hardhat-gas-reporter"
-import "dotenv/config"
-import "solidity-coverage"
-import "hardhat-deploy"
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import "@typechain/hardhat";
+import "@nomicfoundation/hardhat-verify";
+import "@nomicfoundation/hardhat-ethers";
+import "hardhat-gas-reporter";
+import "solidity-coverage";
+import "hardhat-deploy";
+import "dotenv/config";
 
 const config: HardhatUserConfig = {
-    solidity: "0.8.24",
-}
+  solidity: "0.8.8",
+};
 
-export default config
+export default config;
