a4e944068e3142d482ffaa94f652254ef5632aad
Waseem <wasim_ahmad2003@yahoo.com>
2024-08-10 14:36:27 +0500

    configured the app for linting

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,15 @@
-import { HardhatUserConfig } from "hardhat/config";
+import { HardhatUserConfig } from "hardhat/config"
+import "@typechain/hardhat"
+import "@nomiclabs/hardhat-waffle"
+import "@nomiclabs/hardhat-etherscan"
+import "@nomiclabs/hardhat-ethers"
+import "hardhat-gas-reporter"
+import "dotenv/config"
+import "solidity-coverage"
+import "hardhat-deploy"
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.24",
-};
+    solidity: "0.8.24",
+}
+
+export default config
