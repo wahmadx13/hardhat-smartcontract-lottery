7c46f4b8f93bc19f316152fca3e1e3e19f9b4cd0
Waseem <wasim_ahmad2003@yahoo.com>
2024-08-10 19:32:03 +0500

    added logic for getting random number

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -1,18 +1,38 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.8;
 
+import {VRFConsumerBaseV2} from "@chainlink/contracts/src/v0.8/vrf/VRFConsumerBaseV2.sol";
+import {VRFCoordinatorV2Interface} from "@chainlink/contracts/src/v0.8/vrf/interfaces/VRFCoordinatorV2Interface.sol";
+
 error Raffle__NotEnoughETHEntered();
 
-contract Raffle {
+abstract contract Raffle is VRFConsumerBaseV2 {
   /* State Variables */
   uint256 private immutable i_enterenceFee;
   address payable[] private s_players;
+  VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
+  bytes32 private immutable i_gasLane;
+  uint64 private immutable i_subscriptionId;
+  uint32 private immutable i_callbackGasLimit;
+  uint16 private constant REQUEST_CONFIRMATIONS = 3;
+  uint32 private constant NUM_WORDS = 1;
 
   /* Events */
   event RaffleEnter(address indexed palyer);
+  event RequestedRaffleWinner(uint256 indexed requestId);
 
-  constructor(uint256 entrenceFee) {
+  constructor(
+    address vrfCoordinatorV2,
+    uint256 entrenceFee,
+    bytes32 gasLane,
+    uint64 subscriptionId,
+    uint32 callbackGasLimit
+  ) VRFConsumerBaseV2(vrfCoordinatorV2) {
     i_enterenceFee = entrenceFee;
+    i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinatorV2);
+    i_gasLane = gasLane;
+    i_subscriptionId = subscriptionId;
+    i_callbackGasLimit = callbackGasLimit;
   }
 
   function enterRaffle() public payable {
@@ -23,6 +43,20 @@ contract Raffle {
     emit RaffleEnter(msg.sender);
   }
 
+  function requestRandomWinner() external {
+    uint256 requestId = i_vrfCoordinator.requestRandomWords(
+      i_gasLane,
+      i_subscriptionId,
+      REQUEST_CONFIRMATIONS,
+      i_callbackGasLimit,
+      NUM_WORDS
+    );
+    emit RequestedRaffleWinner(requestId);
+  }
+
+  // function fulfillRandomWord(uint256 requestId, uint256[] memory randomWords) internal override {}
+
+  /* View / Pure Functions */
   function getEnterenceFee() public view returns (uint256) {
     return i_enterenceFee;
   }
