fcf62e719b52635880879d096b78c05406f576fb
Waseem <wasim_ahmad2003@yahoo.com>
2024-08-10 15:18:32 +0500

    added basic raffle enterence logic

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -1,4 +1,28 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.8;
 
-contract Raffle {}
+error Raffle__NotEnoughETHEntered();
+
+contract Raffle {
+  /* State Variables */
+  uint256 private immutable i_enterenceFee;
+  address payable[] private s_players;
+
+  constructor(uint256 entrenceFee) {
+    i_enterenceFee = entrenceFee;
+  }
+
+  function enterRaffle() public payable {
+    if (msg.value < i_enterenceFee) {
+      revert Raffle__NotEnoughETHEntered();
+    }
+  }
+
+  function getEnterenceFee() public view returns (uint256) {
+    return i_enterenceFee;
+  }
+
+  function getPlayer(uint256 index) public view returns (address) {
+    return s_players[index];
+  }
+}
