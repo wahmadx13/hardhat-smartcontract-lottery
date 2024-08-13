83cc642aa1f177e2ce785e577f09fafba32e66b0
Waseem <wasim_ahmad2003@yahoo.com>
2024-08-13 15:22:36 +0500

    got everything needed in raffle.sol constructor function needed

--- deploy/01-deploy-raffle.ts
+++ deploy/01-deploy-raffle.ts
@@ -1,7 +1,13 @@
 import { DeployFunction } from "hardhat-deploy/types"
 import { HardhatRuntimeEnvironment } from "hardhat/types"
 import { ethers, network } from "hardhat"
-import { developmentChains, networkConfig } from "../helper-hardhat-config"
+import {
+  developmentChains,
+  networkConfig,
+  VERIFICATION_BLOCK_CONFIRMATIONS,
+} from "../helper-hardhat-config"
+
+const VRF_SUB_FUND_AMOUNT = ethers.parseEther("30")
 
 const deployRaffle: DeployFunction = async function ({
   getNamedAccounts,
@@ -10,21 +16,41 @@ const deployRaffle: DeployFunction = async function ({
   const { deploy, log } = deployments
   const { deployer } = await getNamedAccounts()
   const chainId = network.config.chainId
-  let vrfCoordinatorV2Address: string | undefined
+  let vrfCoordinatorV2Address: string | undefined,
+    vrfCoordinatorV2Mock: any,
+    subscriptionId: string | undefined
 
   if (developmentChains.includes(network.name)) {
-    const vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock")
-    vrfCoordinatorV2Address = await vrfCoordinatorV2Mock.getAddress()
+    const signer = await ethers.getSigner(deployer)
+    vrfCoordinatorV2Mock = await deployments.get("VRFCoordinatorV2Mock")
+    vrfCoordinatorV2Address = vrfCoordinatorV2Mock.address
+    const vrfCoordinatorInstance = await ethers.getContractAt("VRFCoordinatorV2Mock", signer)
+    const transactionResponse = await vrfCoordinatorInstance.createSubscription()
+    const transactionReceipt = await transactionResponse.wait()
+    subscriptionId = transactionReceipt!.logs[0].topics[1]
+    await vrfCoordinatorV2Mock.fundSubscription(subscriptionId, VRF_SUB_FUND_AMOUNT)
   } else {
     vrfCoordinatorV2Address = networkConfig[chainId!]["vrfCoordinatorV2"]
+    subscriptionId = networkConfig[chainId!]["subscriptionId"]
   }
 
-  const args: any = []
+  const waitBlockConfirmations = developmentChains.includes(network.name)
+    ? 1
+    : VERIFICATION_BLOCK_CONFIRMATIONS
+
+  const args: any = [
+    vrfCoordinatorV2Address,
+    subscriptionId,
+    networkConfig[network.config.chainId!]["gasLane"],
+    networkConfig[network.config.chainId!]["keepersUpdateInterval"],
+    networkConfig[network.config.chainId!]["raffleEntranceFee"],
+    networkConfig[network.config.chainId!]["callbackGasLimit"],
+  ]
   const raffle = await deploy("Raffle", {
     from: deployer,
     log: true,
     args: args,
-    waitConfirmations: networkConfig[network.name].blockConfirmations || 0,
+    waitConfirmations: waitBlockConfirmations,
   })
 }
 
--- helper-hardhat-config.ts
+++ helper-hardhat-config.ts
@@ -1,20 +1,42 @@
+import { ethers } from "hardhat"
+
 export interface networkConfigItem {
-  ethUsdPriceFeed?: string
-  blockConfirmations?: number
+  name?: string
+  subscriptionId?: string
+  gasLane?: string
+  keepersUpdateInterval?: string
+  raffleEntranceFee?: BigInt
+  callbackGasLimit?: string
   vrfCoordinatorV2?: string
 }
 
 export interface networkConfigInfo {
-  [key: string]: networkConfigItem
+  [key: number]: networkConfigItem
 }
 
 export const networkConfig: networkConfigInfo = {
-  localhost: {},
-  hardhat: {},
-  sepolia: {
-    ethUsdPriceFeed: "0x694AA1769357215DE4FAC081bf1f309aDC325306",
-    blockConfirmations: 6,
+  31337: {
+    name: "localhost",
+    subscriptionId: "588",
+    gasLane: "0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c", // 30 gwei
+    keepersUpdateInterval: "30",
+    raffleEntranceFee: ethers.parseEther("0.01"), // 0.01 ETH
+    callbackGasLimit: "500000", // 500,000 gas
+  },
+  11155111: {
+    name: "sepolia",
+    subscriptionId: "588",
+    gasLane: "0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c", // 30 gwei
+    keepersUpdateInterval: "30",
+    raffleEntranceFee: ethers.parseEther("0.01"), // 0.01 ETH
+    callbackGasLimit: "500000", // 500,000 gas
+    vrfCoordinatorV2: "0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625",
+  },
+  1: {
+    name: "mainnet",
+    keepersUpdateInterval: "30",
   },
 }
 
 export const developmentChains = ["hardhat", "localhost"]
+export const VERIFICATION_BLOCK_CONFIRMATIONS = 6
